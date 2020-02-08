5cd158be8eacfcbaea328c65376beb152acbbb74
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-08 00:14:14 -0300

    feat: added CRUD endpoint for Lead model

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Lead
+from .serializers import LeadSerializer
 
-# Create your views here.
+
+class LeadListCreate(generics.ListCreateAPIView):
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
