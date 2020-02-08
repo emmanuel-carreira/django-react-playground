4c51656fd202e58e916dbb0f86b44a794741545c
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-07 23:02:04 -0300

    feat: added initial Lead model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    """
+    A simple model for experimentations.
+    """
+    name = models.CharField(max_length=100, null=True, blank=False)
+    email = models.EmailField(null=True, blank=False)
+    message = models.CharField(max_length=300, null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
