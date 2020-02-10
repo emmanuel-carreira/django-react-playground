6a51ce1be57edd730d91303c80a757b053024aa6
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-10 20:23:03 -0300

    feat: added Submit component in the end of page

--- frontend/src/macros/components/App_macros.js
+++ frontend/src/macros/components/App_macros.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import Input_grams from "./Input_grams"
+import Submit from "./Submit"
 
 class App_macros extends Component {
   constructor(props) {
     super(props);
+  }
+
+  send(event){
+    // Makes some ajax request here. WIP.
   }
 
   render () {
@@ -14,6 +19,7 @@ class App_macros extends Component {
         <Input_grams key="protein_input" input_name="Proteína"/>
         <Input_grams key="fat_input" input_name="Gordura"/>
         <Input_grams key="carb_input" input_name="Carboidratos"/>
+        <Submit key="submit_button" name="macros" func={this.send}/>
       </div>
     );
   }
