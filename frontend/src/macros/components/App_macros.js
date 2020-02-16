1a8a0a204ae02df28931188b16ba574ed6ad86ae
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-16 19:43:11 -0300

    feat: added setValue function to handle Input_grams change

--- frontend/src/macros/components/App_macros.js
+++ frontend/src/macros/components/App_macros.js
@@ -7,7 +7,18 @@ class App_macros extends Component {
   constructor(props) {
     super(props);
 
-    this.send = this.send.bind(this)
+    this.state = {
+      proteins: '',
+      fats: '',
+      carbohydrates: '',
+    };
+
+    this.send = this.send.bind(this);
+    this.setValue = this.setValue.bind(this);
+  }
+
+  setValue (key, value) {
+    this.setState({[key]: value});
   }
 
   send(event){
@@ -18,9 +29,9 @@ class App_macros extends Component {
     return (
       <div>
         <h1>Me fala teus macros!</h1>
-        <Input_grams key="protein_input" input_name="Proteína"/>
-        <Input_grams key="fat_input" input_name="Gordura"/>
-        <Input_grams key="carb_input" input_name="Carboidratos"/>
+        <Input_grams key="protein_input" input_name="Proteína" func={this.setValue} input_key='proteins'/>
+        <Input_grams key="fat_input" input_name="Gordura" func={this.setValue} input_key='fats'/>
+        <Input_grams key="carb_input" input_name="Carboidratos" func={this.setValue} input_key='carbohydrates'/>
         <Submit key="submit_button" name="macros" func={this.send}/>
       </div>
     );
