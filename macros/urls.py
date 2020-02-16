b14ba36c82bfc71f0c6dc09fa49f30c42c0de39e
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-16 20:40:34 -0300

    feat: added calculate_calories endpoit

--- macros/urls.py
+++ macros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from frontend.views import macros_index
+from .views import calculate_calories
 
 urlpatterns = [
-    path('macros/', macros_index)
+    path('macros/', macros_index),
+    path('macros/calculate_calories', calculate_calories)
 ]
--- macros/views.py
+++ macros/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@csrf_exempt
+@api_view(['POST'])
+def calculate_calories(request):
+    """
+    View used to calculate total calories based on macronutrients quantities
+    received throught POST method.
+    """
+    response = {
+        'message': 'Missing macros information!',
+        'success': False
+    }
+
+    data = request.data
+    proteins = data.get('proteins', None)
+    fats = data.get('fats', None)
+    carbohydrates = data.get('carbohydrates', None)
+
+    if(not proteins or not fats or not carbohydrates):
+        return JsonResponse(response, status=400)
+
+    calories = (int(proteins) * 4) + (int(fats) * 9) + (int(carbohydrates) * 4)
+    response['calories'] = str(calories)
+    response['message'] = 'Calories calculated successfully!'
+    response['success'] = True
+    return JsonResponse(response, status=200)
