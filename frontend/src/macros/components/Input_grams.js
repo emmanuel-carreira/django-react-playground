faf2f190ec0cf472eb9e2e8cd7ee7a00833825fa
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-02-16 19:31:51 -0300

    feat: added handleChange function to Input_grams component

--- frontend/src/macros/components/Input_grams.js
+++ frontend/src/macros/components/Input_grams.js
@@ -3,13 +3,29 @@ import React, { Component } from "react";
 class Input_grams extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (event) {
+    this.setState({value: event.target.value});
   }
 
   render() {
     return (
       <div>
         <h3>{this.props.input_name}</h3>
-        <input type="text" placeholder="Quantidade em gramas"></input>
+        <input
+          type="text"
+          placeholder="Quantidade em gramas"
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+        </input>
       </div>
     );
   }
